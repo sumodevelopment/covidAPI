2f6f7303909d5bcdc04227c2b59dea806325675b
Dewald Els <dewaldifels@gmail.com>
2020-03-16 22:06:45 +0100

    Incorrect data being returned :(

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
+// Imports
 const express = require("express");
-const app = express();
 const db = require("quick.db");
 const cors = require('cors');
-
+// constants
 const { PORT = 3000 } = process.env;
+// Instances
+const app = express();
 
 app.use(cors());
 
+// Functions
 const { getAllRequest, getCountriesRequest } = require('./requests');
 
-setInterval(getAllRequest, 600000);
-setInterval(getCountriesRequest, 600000);
+getAllRequest();
+getCountriesRequest();
+
+setInterval(() => {
+    getAllRequest();
+}, 600000);
+
+setInterval(() => {
+    getCountriesRequest();
+}, 600000);
 
 app.get('/update', (req, res) => {
     getAllRequest();
@@ -18,13 +29,12 @@ app.get('/update', (req, res) => {
     res.send('Updated from sources...');
 });
 
-
-app.get("/all/", async function (req, res) {
+app.get("/all", async function (req, res) {
     let all = await db.fetch("all");
     res.send(all);
 });
 
-app.get("/countries/", async function (req, res) {
+app.get("/countries", async function (req, res) {
     let countries = await db.fetch("countries");
     res.send(countries);
 });
